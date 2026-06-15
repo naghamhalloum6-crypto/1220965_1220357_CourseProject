34954d5c99844660905c0a758069dec0c79ba847
Nagham Halloum <naghamhalloum6@gmail.com>
2026-06-15 04:51:35 -0700

    create animated splash screen

---
+++ travelplanner/app/src/main/java/com/example/travelplanner/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.travelplanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imgLogo = findViewById(R.id.imgLogo);
+        TextView txtAppName = findViewById(R.id.txtAppName);
+        TextView txtSubtitle = findViewById(R.id.txtSubtitle);
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+
+        imgLogo.startAnimation(fadeIn);
+        txtAppName.startAnimation(fadeIn);
+        txtSubtitle.startAnimation(fadeIn);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, IntroductionActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
