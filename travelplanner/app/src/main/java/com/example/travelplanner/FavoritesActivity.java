cb45a7cc8e3d57eb64afa16628d7b68635dd2a78
WoroudHamayel <worod12.amjad@gamil.com>
2026-06-19 04:33:28 +0300

    setup favorites recycler view

--- travelplanner/app/src/main/java/com/example/travelplanner/FavoritesActivity.java
+++ travelplanner/app/src/main/java/com/example/travelplanner/FavoritesActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -15,6 +17,13 @@ public class FavoritesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_favorites);
+        RecyclerView recyclerFavorites =
+                findViewById(R.id.recyclerFavorites);
+
+// Prepare favorites list layout
+        recyclerFavorites.setLayoutManager(
+                new LinearLayoutManager(this)
+        );
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
